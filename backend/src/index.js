6e4650398f79369a7d2facfba3c03fc94cd6f46f
Jatin Gupta <jatingupta70089@gmail.com>
2025-08-02 14:24:29 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,6 +3,9 @@ require('dotenv').config();
 const connectDB= require('./lib/db');
 const cookieParser= require('cookie-parser')
 const cors= require('cors');
+
+const {app, server, io}= require('./lib/socket');
+
 app.use(cors({
     origin: "https://chat-application-6-edm0.onrender.com",
     credentials: true
@@ -13,7 +16,6 @@ const PORT= process.env.PORT || 5001
 const authRoutes= require('./routes/authRoutes');
 const messageRoutes= require('./routes/messageRoutes');
 
-const {app, server, io}= require('./lib/socket');
 
 app.use(express.json({ limit: '50mb' }));
 app.use(cookieParser())
