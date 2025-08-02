409f71f807d68b10f8890640ab0bd9c0ab6612de
Jatin Gupta <jatingupta70089@gmail.com>
2025-08-02 14:36:18 +0530

    Update axios.js

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance= axios.create({
-    baseURL: "https://chat-application-3-ruuz.onrender.com",
+    baseURL: "https://chat-application-backend-rqwp.onrender.com",
     withCredentials: true
 })
